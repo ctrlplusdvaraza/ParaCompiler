fad6f68939c22cee157c1f17aa61fc4189ce3bb1
Aleksandr <baas157@bk.ru>
2025-11-14 12:12:44 +0300

    chore: output invalid tests

--- tests/test_for_parser.sh
+++ tests/test_for_parser.sh
@@ -5,21 +5,38 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
+GREEN="\033[32m"
+RED="\033[31m"
+CYAN="\033[36m"
+YELLOW="\033[33m"
+BOLD="\033[1m"
+RESET="\033[0m"
+
 program=$1
 folder="./erroneous_tests_for_parser"
 
+printf "${GREEN}Start tests in${RESET} $folder\n"
+n_all=0
+n_falled=0
 for file in "$folder"/*; do
   if [ -f "$file" ]; then
-    echo "▶ Запуск '$program' с файлом '$file'..."
     "$program" "$file" > /dev/null 2>&1
     status=$?
-
-    if [ $status -ne 0 ]; then
-      echo "❌ Программа завершилась с ошибкой ($status) на файле: $file"
-    else
-      echo "✅ Программа завершилась успешно на файле: $file"
+    ((n_all++))
+    if [ $status -eq 0 ]; then
+      ((n_falled++))
+      printf "${RED}Test should not have been compiled${RESET}\n$file\n"
+      echo "-----------------------------------------------------------"
+      cat "$file"
+      echo "-----------------------------------------------------------"
     fi
-    echo "------------------------------------"
   fi
 done
+
+if [ $n_falled -eq 0 ]; then
+    printf "${GREEN}All $n_all tests in${RESET} $folder ${GREEN}passed${RESET}\n"
+else 
+    printf "${RED}$n_falled/$n_all tests in${RESET} $folder ${RED}falled${RESET}\n"
+fi
+
 
